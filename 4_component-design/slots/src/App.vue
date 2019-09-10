f1793fd04877dd08146c8b17c5dbadec2cd8b777
imjehoon_mac <imjehoon@naver.com>
2019-09-10 22:44:21 +0900

    slots add

---
+++ 4_component-design/slots/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  <ul>
+    <list-item style="color:red;">
+      item1
+    </list-item>
+    <list-item>
+      <p>item 2</p>
+      <span>hello</span>
+    </list-item>
+  </ul>
+  <!-- <list-item item="item1" ></list-item>
+  <list-item item="item2" ></list-item>
+  <list-item item="item3" ></list-item> -->
+</div>
+  
+</template>
+
+<script>
+ import ListItem from './components/ListItem.vue';
+ 
+  export default {
+    components:{
+      'list-item': ListItem,
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
